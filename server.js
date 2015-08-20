7d5b2f202aee127a144afc41944b9dd28cbf0443
Bruno Calheira <bruno@calheira.com>
2015-08-20 10:24:17 -0300

    blocking operation

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ function start (route, handle) {
 	function onRequest(request, response){
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for " + pathname +" received!");
-		route(handle, pathname);
+
 		response.writeHead(200, {"Content-Type": "text/plain"});
-		response.write("Oi NodeJS! Versao 2");
+		var content = route(handle, pathname);
+		response.write(content);
 		response.end();
 	}
 	http.createServer(onRequest).listen(8888);
